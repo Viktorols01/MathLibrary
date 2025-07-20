5485c7de44035690ad5915255e2421b7304316b9
Viktor Ols <viktorols01@gmail.com>
2025-07-20 23:46:50 +0200

    format using clangd

--- include/Matrix.h
+++ include/Matrix.h
@@ -3,105 +3,79 @@
 
 #include <cstddef>
 
-namespace MathLibrary
-{
+namespace MathLibrary {
 
-    // It appears I must implement the functions
-    // here for the template to work?
-    // Will have to study this in more detail later
-    template <std::size_t rowCount, std::size_t colCount>
-    class Matrix
-    {
-    private:
-        float *components;
+// It appears I must implement the functions
+// here for the template to work?
+// Will have to study this in more detail later
+template <std::size_t rowCount, std::size_t colCount> class Matrix {
+private:
+  float *components;
 
-    public:
-        Matrix()
-        {
-            components = new float[rowCount * colCount]();
-        }
-        ~Matrix()
-        {
-            delete[] components;
-        }
+public:
+  Matrix() { components = new float[rowCount * colCount](); }
+  ~Matrix() { delete[] components; }
 
-        // No checking if operation valid --> better performance?
-        float get(int row, int col) const
-        {
-            return components[row + rowCount * col];
-        }
-        void set(int row, int col, float value)
-        {
-            components[row + rowCount * col] = value;
-        }
+  // No checking if operation valid --> better performance?
+  float get(int row, int col) const { return components[row + rowCount * col]; }
 
-        Matrix<colCount, rowCount> transpose() const {
-            Matrix<colCount, rowCount> result;
-            for (int i = 0; i < rowCount; i++)
-            {
-                for (int j = 0; j < colCount; j++)
-                {
-                    result.set(j, i, get(i, j));
-                }
-            }
-            return result;
-        }
+  void set(int row, int col, float value) {
+    components[row + rowCount * col] = value;
+  }
 
-        Matrix operator+(const Matrix &other) const
-        {
-            Matrix result;
-            for (int i = 0; i < rowCount; i++)
-            {
-                for (int j = 0; j < colCount; j++)
-                {
-                    result.set(i, j, get(i, j) + other.get(i, j));
-                }
-            }
-            return result;
+  Matrix<colCount, rowCount> transpose() const {
+    Matrix<colCount, rowCount> result;
+    for (int i = 0; i < rowCount; i++) {
+      for (int j = 0; j < colCount; j++) {
+        result.set(j, i, get(i, j));
+      }
+    }
+    return result;
+  }
+
+  Matrix operator+(const Matrix &other) const {
+    Matrix result;
+    for (int i = 0; i < rowCount; i++) {
+      for (int j = 0; j < colCount; j++) {
+        result.set(i, j, get(i, j) + other.get(i, j));
+      }
+    }
+    return result;
+  }
+  Matrix operator-(const Matrix &other) const {
+    Matrix result;
+    for (int i = 0; i < rowCount; i++) {
+      for (int j = 0; j < colCount; j++) {
+        result.set(i, j, get(i, j) - other.get(i, j));
+      }
+    }
+    return result;
+  }
+  Matrix operator*(int index) const {
+    Matrix result;
+    for (int i = 0; i < rowCount; i++) {
+      for (int j = 0; j < colCount; j++) {
+        result.set(i, j, get(i, j) * index);
+      }
+    }
+    return result;
+  }
+  template <std::size_t colCountOther>
+  Matrix<rowCount, colCountOther>
+  operator*(const Matrix<colCount, colCountOther> &other) {
+    Matrix<rowCount, colCountOther> result;
+    for (int i = 0; i < rowCount; i++) {
+      for (int j = 0; j < colCountOther; j++) {
+        float sum = 0;
+        for (int k = 0; k < colCount; k++) {
+          sum += get(i, k) * other.get(k, j);
         }
-        Matrix operator-(const Matrix &other) const
-        {
-            Matrix result;
-            for (int i = 0; i < rowCount; i++)
-            {
-                for (int j = 0; j < colCount; j++)
-                {
-                    result.set(i, j, get(i, j) - other.get(i, j));
-                }
-            }
-            return result;
-        }
-        Matrix operator*(int index) const
-        {
-            Matrix result;
-            for (int i = 0; i < rowCount; i++)
-            {
-                for (int j = 0; j < colCount; j++)
-                {
-                    result.set(i, j, get(i, j) * index);
-                }
-            }
-            return result;
-        }
-        template <std::size_t colCountOther>
-        Matrix<rowCount, colCountOther> operator*(const Matrix<colCount, colCountOther> &other)
-        {
-            Matrix<rowCount, colCountOther> result;
-            for (int i = 0; i < rowCount; i++)
-            {
-                for (int j = 0; j < colCountOther; j++)
-                {
-                    float sum = 0;
-                    for (int k = 0; k < colCount; k++)
-                    {
-                        sum += get(i, k) * other.get(k, j);
-                    }
-                    result.set(i, j, sum);
-                }
-            }
-            return result;
-        }
-    };
-}
+        result.set(i, j, sum);
+      }
+    }
+    return result;
+  }
+};
+} // namespace MathLibrary
 
 #endif
--- src/tests/MatrixTest.cpp
+++ src/tests/MatrixTest.cpp
@@ -1,64 +1,55 @@
-#include "Matrix.h"
+#include "Matrix.h" 
 #include <gtest/gtest.h>
 
-TEST(CreateMatrixTest, CreateEmptyMatrix)
-{
-    MathLibrary::Matrix<2, 2> m;
-    for (int i = 0; i < 2; i++)
-    {
-        for (int j = 0; j < 2; j++)
-        {
-            EXPECT_EQ(m.get(i, j), 0);
-        }
+TEST(CreateMatrixTest, CreateEmptyMatrix) {
+  MathLibrary::Matrix<2, 2> m;
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      EXPECT_EQ(m.get(i, j), 0);
     }
+  }
 };
 
 class MatrixTest : public testing::Test {
-    protected:
-    MathLibrary::Matrix<2, 3> m;
-    MathLibrary::Matrix<3, 1> v;
-    float eps;
+protected:
+  MathLibrary::Matrix<2, 3> m;
+  MathLibrary::Matrix<3, 1> v;
+  float eps;
 
-    MatrixTest() {
-        eps = 0.0001;
-        m.set(0, 0, 7.0);
-        v.set(0, 0, 1.0);
-        v.set(0, 0, 1.0);
-    }
-
+  MatrixTest() {
+    eps = 0.0001;
+    m.set(0, 0, 7.0);
+    v.set(0, 0, 1.0);
+    v.set(0, 0, 1.0);
+  }
 };
 
 TEST_F(MatrixTest, ModifyMatrixTest) {
-    EXPECT_EQ(m.get(0, 0), 7.0);    
-    EXPECT_EQ(m.get(0, 1), 0.0);    
-    EXPECT_EQ(m.get(1, 0), 0.0);    
-    EXPECT_EQ(m.get(0, 1), 0.0);    
-    
-    m.set(1, 1, 3.0);
+  EXPECT_EQ(m.get(0, 0), 7.0);
+  EXPECT_EQ(m.get(0, 1), 0.0);
+  EXPECT_EQ(m.get(1, 0), 0.0);
+  EXPECT_EQ(m.get(0, 1), 0.0);
 
-    EXPECT_EQ(m.get(1, 1), 3.0);    
+  m.set(1, 1, 3.0);
+
+  EXPECT_EQ(m.get(1, 1), 3.0);
 }
 
 TEST_F(MatrixTest, TransposeAndMultiplyTest) {
-    
-    // Create transpose and multiply with original
-    auto m_prime = m.transpose();
-    auto mm = m_prime*m;
-    
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (i == 0 && j == 0) {
-                EXPECT_NEAR(mm.get(i, j), 49.0, eps);
-            } else {
-                EXPECT_NEAR(mm.get(i, j), 0.0, eps); // Won't work due to precision of float
-            }
-        }
+
+  // Create transpose and multiply with original
+  auto m_prime = m.transpose();
+  auto mm = m_prime * m;
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (i == 0 && j == 0) {
+        EXPECT_NEAR(mm.get(i, j), 49.0, eps);
+      } else {
+        EXPECT_NEAR(mm.get(i, j), 0.0, eps);
+      }
     }
-    
-    
-    // Multiply m with v
+  }
 
-
+  // Multiply m with v
 }
